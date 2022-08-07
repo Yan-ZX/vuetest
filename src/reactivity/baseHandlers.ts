f74f47df341765bb3fd9066d5005fad94ff596ee
yanzx <2453599757@qq.com>
2022-08-07 22:39:16 +0800

    code ref

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,4 +1,34 @@
+import { track, trigger } from './effect';
+import { readonly } from './reactive';
 
-export const mutableHandler = {
-    
+
+function createGetter(isReadonly = false) {
+    return function get(target, key) {
+        const res = Reflect.get(target, key);
+        //TODO 依赖收集
+        if (!isReadonly) {
+            track(target, key);
+        }
+        return res
+    }
+}
+
+function createSetter() {
+    return function set(target, key, value) {
+        const res = Reflect.set(target, key, value) ;
+        //TODO 触发依赖
+        trigger(target, key);
+        return res;
+    }
+}
+
+export const mutableHandlers = {
+    get: createGetter(),
+    set: createSetter(),
+}
+export const readonlyHandlers = {
+    get: createGetter(true),
+    set(target, key, value) {
+        return true
+    }
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,56 +1,16 @@
 import { track, trigger } from "./effect";
+import { mutableHandlers, readonlyHandlers } from './baseHandlers';
 
-function createGetter(isReadonly = false) {
-    return function get(target, key) {
-        const res = Reflect.get(target, key);
-        //TODO 依赖收集
-        if (!isReadonly) {
-            track(target, key);
-        }
-        return res
-    }
-}
 
-function createSetter() {
-    return function set(target, key, value) {
-        const res = Reflect.set(target, key, value) ;
-        //TODO 触发依赖
-        trigger(target, key);
-        return res;
-    }
-}
 
 export function reactive(raw) {
-    return new Proxy(raw, {
-        get: createGetter(),
-        // get(target, key) {
-        //     const res = Reflect.get(target, key);
-        //     //TODO 依赖收集
-        //     track(target, key);
-        //     return res
-        // },
-        set:createSetter()
-        // set(target, key, value) {
-        //     const res = Reflect.set(target, key, value) ;
-        
-        //     //TODO 触发依赖
-        //     trigger(target, key);
-        //     return res;
-        // }
-    })
+
+    return new Proxy(raw, mutableHandlers)
+    
 }
 
 export function readonly(raw) {
-    return new Proxy(raw, {
-        get:createGetter(true),
-        // get(target, key) {
-        //     const res = Reflect.get(target, key);
-        //     return res
-        // },
+    
+    return new Proxy(raw, readonlyHandlers)
 
-        set(target, key, value) {
-            
-            return true;
-        }
-    })
 }
